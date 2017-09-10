c43b6288621c0e7c1c3cf381fdd15af7d5b665f9
Ethan <elawrence152@gmail.com>
2017-09-09 20:30:35 -0400

    works now boiz

--- src/modules/commands/info/idd.js
+++ src/modules/commands/info/idd.js
@@ -3,23 +3,28 @@ const Logger = require('../../../util/Logger');
 
 function exec(message) {
 
-const used = message.member || author;
-const args = message.content.split(/\s+/).slice(1);
-
-    if(args[0]=="user"){
-      if (!args[1]) return message.reply("No user given :<");
-      try{
- 	let member = message.mentions.members.first();       
-    message.reply(`${member}`);
-      }catch(err){
-        return message.reply(`Invalid user specified`);
-      }
-    }else if(args[0]=="server"){
-message.reply(`${message.guild.id}`);
-  }else if(args[0]=="channel"){
-    message.reply(`${message.guild.channel.id}`); 
-  }
-  }
+    
+    const args = message.content.split(/\s+/).slice(1);
+    
+        if (!args[0]) 
+            return message.channel.send("No action given.").catch(console.error);
+         
+        if(args[0]=="user"){
+        let member = message.mentions.members.first();
+        if(!member)
+            return message.reply("Please mention a valid member of this server");
+            if(args[1]==`${member}`){
+                return message.channel.send("User ID:"+"`"+ `${member.id}`+"`").catch(console.error);
+        }    }
+        
+        else if(args[0]=="server"){
+            return message.channel.send("Server ID:"+"`"+ `${message.guild.id}`+"`").catch(console.error);
+            } else 
+                
+            if(args[0]=="channel"){
+                return message.channel.send("Channel ID:"+"`"+ `${message.channel.id}`+"`").catch(console.error);
+            }
+    };
 
 module.exports = new Command('idd', exec, {
     aliases: ['idd'],
@@ -32,3 +37,4 @@ module.exports = new Command('idd', exec, {
     channelRestriction: 'guild',
     category: 'info'
 });
+

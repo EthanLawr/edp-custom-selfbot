8685ed399ac81162270f42c7aeda0787f3876fb4
Ethan <elawrence152@gmail.com>
2017-09-09 20:26:38 -0400

    Update help.js

--- src/modules/commands/util/help.js
+++ src/modules/commands/util/help.js
@@ -1,6 +1,7 @@
 const { Command } = require('discord-akairo');
 const Discord = require ('discord.js');
 
+//Categories
 const utility = "" +
 "**Async** - Something Like Eval but better\n" +
 "**Eval** - Something for progammers basically\n" +
@@ -43,22 +44,91 @@ const info = "" +
 "**Stats** - Shows the bot's statistics\n" +
 "**User** - Shows information about a user\n";
 
+const moderation = "" +
+"**Ban** - Ban someone by mentioning them\n" +
+"**Kick** - Kick someone by mentioning them\n";
+
+//Commands Constants
+const ColorDesc = "" +
+"Generates a random color or outputs with a specified color.";
+
+const ColorUsage = "" + 
+"//color [Hex Code]\n" +
+"\t╰Produces a color based on what hex code you input\n" +
+"//color[Integer]\n" +
+"\t╰Produces a color based on what integer you input\n" +
+"//color [Random Stuff]\n" +
+"\t╰Produces a random color\n" +
+"//color[auto]\n" +
+"\t╰Makes it so that the color is equal to your current role";
+
+
 const time = new Date();
+
 function exec(message, args){
-    args.amount = Math.min(args.amount, 200);
+	
+	const urgs = message.content.split(/\s+/).slice(1);
+	
+	if (!urgs[0]) {
+	message.delete();
+    return message.channel.sendEmbed(new Discord.RichEmbed().setDescription("Type //help [command] to learn how to use a command! (coming soon)").setColor("#2200FF").setTitle(`Help Menu`).addField("Want all of the help commands?", "Type `//help all`").addField("Want all of the fun help commands?", "Type `//help fun`").addField("Want all of the utility help commands?", "Type `//help utility` or `//help util`").addField("Want all of the info help commands?", "Type `//help information` or `//help info`").addField("Want all of the moderation help commands?", "Type `//help moderation` or `//help mod`"));	
+    }
+	
+    if (urgs[0]=="all") {
+	args.amount = Math.min(args.amount, 200);
     let ms;
     let delay=500;
     let currentTime;
     for(let i=1;i<args.amount+1;i++){
-      message.channel.sendEmbed(new Discord.RichEmbed().setDescription("Type //help [command] to learn how to use a command! (coming soon)").setColor("#2200FF").setTitle(`Help Menu`).addField("Fun Help Menu", fun).addField("Info Help Menu", info).addField("Utility Help Menu", utility));
+      message.channel.sendEmbed(new Discord.RichEmbed().setDescription("Type //help [command] to learn how to use a command! (coming soon)").setColor("#2200FF").setTitle(`Help Menu`).addField("Fun Help Menu", fun).addField("Info Help Menu", info).addField("Utility Help Menu", utility).addField("Moderation Help Menu", moderation));
       ms=time.getMilliseconds();
 
       do
       {
         currentTime=time.getMilliseconds;
       }while((currentTime)-ms<delay);
+    }  message.delete();
+	  }
+	  
+	if (urgs[0]=="fun") {
+	message.delete();
+    return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(fun).setColor("#2200FF").setTitle(`Fun Help Menu`));
     }
-    message.delete();
+	
+	if (urgs[0]=="information") {
+	message.delete();
+    return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(info).setColor("#2200FF").setTitle(`Information Help Menu`));
+    }
+	
+	if (urgs[0]=="info") {
+	message.delete();
+    return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(info).setColor("#2200FF").setTitle(`Info Help Menu`));
+    }
+	
+	if (urgs[0]=="utility") {
+	message.delete();
+    return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(utility).setColor("#2200FF").setTitle(`Utility Help Menu`));
+    }
+	
+	if (urgs[0]=="util") {
+	message.delete();
+    return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(utility).setColor("#2200FF").setTitle(`Util Help Menu`));
+    }
+	
+	if (urgs[0]=="moderation") {
+	message.delete();
+    return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(moderation).setColor("#2200FF").setTitle(`Moderation Help Menu`));
+    }
+	
+	if (urgs[0]=="mod") {
+	message.delete();
+    return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(moderation).setColor("#2200FF").setTitle(`Mod Help Menu`));
+    }
+	
+	if (urgs[0]=="color") {
+	message.delete();
+    return message.channel.sendEmbed(new Discord.RichEmbed().setDescription("Beta Thing").setColor("#2200FF").setTitle(`Singular Command Help Menu`).addField("Color", ColorDesc).addField("Aliases:", "Color\n"+"Colour").addField("Usage:", ColorUsage));
+    }
 }
 
 module.exports = new Command('help', exec, {

5cf2520782f3b281ad2c90da9a64c63fdef1e3d3
Ethan <elawrence152@gmail.com>
2017-10-07 23:18:13 -0400

    Cats and Dogs no longer have embeds!

--- src/modules/commands/fun/cat.js
+++ src/modules/commands/fun/cat.js
@@ -1,22 +1,23 @@
 const { Command } = require('discord-akairo');
-const Discord = require ('discord.js');
-
 const got = require('got');
-getCat = (callback) => {
-  got('http://www.random.cat/meow').then(res => {
-    try {
-      callback(undefined, JSON.parse(res.body).file);
-    } catch (err) {
-      callback(err);
-    }
-  }).catch(callback);
+const snekfetch = require('snekfetch');
+
+const getCat = () => {
+  return new Promise( (resolve,rej)=>{
+    got('http://www.random.cat/meow').then(res => {
+      try{
+        const f = JSON.parse(res.body).file;
+        (f!=null)?resolve(f):rej('File not found');
+      }catch(err){
+        rej(err);
+      }
+    }).catch(rej);
+  });
 };
 
-function exec(message, args) {
-    return getCat((a,b)=>message.channel.sendEmbed(new Discord.RichEmbed()
-	.setColor('#2200FF')
-	.setImage(b)));
-}
+async function exec(message, channel) {
+	await message.channel.send('',{files: [{attachment: (await snekfetch.get(await getCat())).body}]}).then(message.delete());
+};
 
 module.exports = new Command('cat', exec, {
     aliases: ['cat', 'kitty'],
--- src/modules/commands/fun/dog.js
+++ src/modules/commands/fun/dog.js
@@ -1,21 +1,22 @@
 const { Command } = require('discord-akairo');
-const Discord = require ('discord.js');
-
+const snekfetch = require('snekfetch');
 const got = require('got');
-getDog = (callback) => {
-  got('https://random.dog/woof.json').then(res => {
-    try {
-      callback(undefined, JSON.parse(res.body).url);
-    } catch (err) {
-      callback(err);
-    }
-  }).catch(callback);
+
+const getDog = () => {
+  return new Promise( (resolve,rej)=>{
+    got('https://random.dog/woof.json').then(res => {
+      try{
+        const f = JSON.parse(res.body).file;
+        (f!=null)?resolve(f):rej('File not found');
+      }catch(err){
+        rej(err);
+      }
+    }).catch(rej);
+  });
 };
 
-function exec(message, args) {
-    return getDog((a,b)=>message.channel.sendEmbed(new Discord.RichEmbed()
-	.setColor('#2200FF')
-	.setImage(b)));
+async function exec(message, args) {
+    await message.channel.send('',{files: [{attachment: (await snekfetch.get(await getDog())).body}]}).then(message.delete());
 }
 
 module.exports = new Command('dog', exec, {

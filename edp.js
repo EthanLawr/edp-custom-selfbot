3d0d0698a85c18dbc01f918fb3fda2835d7cc192
Ethan <elawrence152@gmail.com>
2017-10-23 01:58:06 -0400

    Delete edp.js

--- edp.js
+++
@@ -1,9 +0,0 @@
-const EdpClient = require('./src/struct/EdpClient');
-const Logger = require('./src/util/Logger');
-
-const config = require('./src/data/config.json');
-const client = new EdpClient(config);
-
-client.start().then(() => {
-    Logger.info('EDP\'s Custom Selfbot is now ready!');
-});

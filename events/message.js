dec993c9fc479fd76e396f3a0deeb41e5912e4c3
Ethan <elawrence152@gmail.com>
2017-10-29 18:06:43 -0400

    Bot is now client. Msg is now message.

--- events/message.js
+++ events/message.js
@@ -2,26 +2,26 @@ const config = require('../config.json');
 const Logger = require('../functions/logger');
 const util = require('util');
 
-module.exports = (bot, msg) => {
-	if(msg.author.id !== bot.user.id) {
+module.exports = (client, message) => {
+	if(message.author.id !== client.user.id) {
 		return;
 	}
-	if(!msg.content.startsWith(config.discord.prefix)) {
+	if(!message.content.startsWith(config.discord.prefix)) {
 		return;
 	}
-	const command = Object.keys(bot.commands).filter((command) => bot.commands[command].commands.indexOf(msg.content.replace(config.discord.prefix, '').split(' ')[0]) > -1);
+	const command = Object.keys(client.commands).filter((command) => client.commands[command].commands.indexOf(message.content.replace(config.discord.prefix, '').split(' ')[0]) > -1);
 	if (command.length > 0) {
-		const args = msg.content.replace(config.discord.prefix, '').split(' ').length > 1 ? msg.content.replace(config.discord.prefix, '').split(' ').slice(1) : [];
+		const args = message.content.replace(config.discord.prefix, '').split(' ').length > 1 ? message.content.replace(config.discord.prefix, '').split(' ').slice(1) : [];
 		try {
 			Logger.cmd(`${command}`);
-			bot.commands[command[0]].execute(bot, msg, args);
+			client.commands[command[0]].execute(client, message, args);
 		} catch(error) {
 			let toSend = 'An error has occured running that command.```js\n' + util.inspect(error) + '```';
 			if(toSend.length > 1900) {
-				msg.edit('An error has occured running that command and the output has been logged to console.');
+				message.edit('An error has occured running that command and the output has been logged to console.');
 				throw new Error(error);
 			} else {
-				msg.edit(toSend);
+				message.edit(toSend);
 				throw new Error(error);
 			}
 		}
